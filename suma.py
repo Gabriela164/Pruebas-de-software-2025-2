259585f58e2a5cf37e0a920e2e705e1232f9c762
Gabriela López <gabriela_164@ciencias.unam.mx>
2025-02-17 21:59:13 -0600

    Agregar funcion de suma inicial

---
+++ suma.py
@@ -0,0 +1,30 @@
+
+''''
+Script que suma dos números enteros 
+Prueba para Pruebas de Software y administración de la configuración
+
+Febrero 2025 
+Autor: Gabriela164
+'''
+
+def suma(a, b):
+    return a + b
+
+print("La suma de 3 y 5 es: ", suma(3, 5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
